11212ee0dcefd6ffa4cb0c3cd2416243c1c40960
jordandowens <165048966+jordandowens@users.noreply.github.com>
2026-03-16 22:19:23 -0500

    .env update

--- backend/api/services/mariadb.py
+++ backend/api/services/mariadb.py
@@ -4,9 +4,9 @@ import os
 # Establish mariadb connection
 def get_connection():
     return mariadb.connect(
-        user=os.getenv("MYSQL_USER", "gymuser"), 
-        password=os.getenv("MYSQL_PASSWORD", "gympass"),
-        host=os.getenv("MYSQL_HOST", "mariadb"),
-        port=3306,
-        database=os.getenv("MYSQL_DATABASE", "gymrag")
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        host=os.getenv("MYSQL_HOST"),
+        port=int(os.getenv("MYSQL_PORT")),
+        database=os.getenv("MYSQL_DATABASE")
     )
